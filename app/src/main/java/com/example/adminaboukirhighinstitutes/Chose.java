230105d6e47625251d63dc3f792349b22c92f2af
Mostafa <deshacomp@gmail.com>
2019-03-10 14:49:42 +0200

    update

--- app/src/main/java/com/example/adminaboukirhighinstitutes/Chose.java
+++ app/src/main/java/com/example/adminaboukirhighinstitutes/Chose.java
@@ -1,10 +1,12 @@
 package com.example.adminaboukirhighinstitutes;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Chose extends AppCompatActivity implements View.OnClickListener {
 
@@ -36,16 +38,31 @@ public class Chose extends AppCompatActivity implements View.OnClickListener {
         switch (v.getId()) {
 
             case R.id.put_new_student:
-                intent = new Intent(this, NewStudentToFirebase.class);
-                intent.putExtra("intent", getIntent);
-                startActivity(intent);
-                break;
+                if (isNetworkConnected()) {
+                    intent = new Intent(this, NewStudentToFirebase.class);
+                    intent.putExtra("intent", getIntent);
+                    startActivity(intent);
+                    break;
+                } else
+                    Toast.makeText(this, "Please connect to the network", Toast.LENGTH_LONG).show();
+
 
             case R.id.show_all_student:
-                intent = new Intent(this, StudentsList.class);
-                intent.putExtra("intent", getIntent);
-                startActivity(intent);
-                break;
+                if (isNetworkConnected()) {
+                    intent = new Intent(this, StudentsList.class);
+                    intent.putExtra("intent", getIntent);
+                    startActivity(intent);
+                    break;
+                } else
+                    Toast.makeText(this, "Please connect to the network", Toast.LENGTH_LONG).show();
+
         }
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+        assert cm != null;
+        return cm.getActiveNetworkInfo() != null;
     }
 }
